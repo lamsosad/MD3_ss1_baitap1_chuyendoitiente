350e3799f08439100ffa6c31344605986b7309c4
Nguyễn Bá Lâm <nguyenbalam2307@gmail.com>
2023-03-07 10:53:15 +0700

    up2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int rate = 23000;
         Scanner money = new Scanner(System.in);
         System.out.println("Nhập giá tiền cần chuyển đổi: ");
         float USD = money.nextFloat();
-        System.out.println("Tiền VNĐ:" + USD * 23000 + "VND");
+        System.out.println("Tiền VNĐ:" + USD * rate + "VND");
     }
 }
